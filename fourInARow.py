224ca674debb0b6855ed9a7aeda942de9b0bc54c
Steven69420 <43243540+steven69420@users.noreply.github.com>
2020-06-30 19:02:29 +0800

    Four In A Row Game
    This is a 4 in a row game.

--- fourInARow.py
+++ fourInARow.py
@@ -7,7 +7,7 @@ BLUE = (0,0,255)
 BLACK = (0,0,0)
 YELLOW = (255,255,0)
 RED = (255, 0, 0)
-ROW_COUNT = 7
+ROW_COUNT = 6
 COLUMN_COUNT = 7
 def create_board():
 	board = np.zeros((ROW_COUNT, COLUMN_COUNT))
